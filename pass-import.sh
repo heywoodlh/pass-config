7c5897dfc4ea8938f70bcf186e10bca6d826892c
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-01-13 22:50:46 +0000

    Finished pass-import.sh

--- pass-import.sh
+++ pass-import.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 
-if [[ -f ~/pass_public.key && -f ~/pass_private.key ]]; then
+if [[ -f ~/.pass_public.key && -f ~/.pass_private.key ]]; then
 
 echo "Pass git repository URL: "
 read GIT_REPO
 
-gpg --import ~/pass_public.key
+gpg --import ~/.pass_public.key
 
-gpg --import ~/pass_private.key
+gpg --import ~/.pass_private.key
 
 #Get GPG key ID
 
-GPG_ID=$(gpg ~/pass_private.key |  grep ssb | cut -d"/" -f2 | cut -d" " -f1 | head -1)
+GPG_ID=$(gpg ~/.pass_private.key |  grep ssb | cut -d"/" -f2 | cut -d" " -f1 | head -1)
 
 pass init $GPG_ID
 
@@ -20,9 +20,23 @@ pass git init
 
 pass git remote add origin $GIT_REPO
 
+pass git pull origin master
+
+printf "\n"
+
+echo "Delete key files? (y/N)"
+read RESPONSE
+
+if [[ $RESPONSE == [yY] ]]; then 
+ rm ~/.pass_p*
+fi
+
+printf "\n"
+echo "Complete!"
+
 else 
 
-echo "Please name pass GPG keys as such: ~/pass_private.key and ~/pass_public.key"
+echo "Please name pass GPG keys as such: ~/.pass_private.key and ~/.pass_public.key"
 
 
 fi
