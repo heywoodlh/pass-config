ad1486377fd233ad8c89ecf16278c272b1465cd3
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-01-12 18:59:17 -0700

    gpg-gen.sh working commit

--- gpg-gen.sh
+++ gpg-gen.sh
@@ -1,55 +1,13 @@
 #!/usr/bin/env bash
 
-### All content below this line is unnecessary ### 
-
-auth() {
-if [ -e ~/.cfze ]
-then 
-  echo "Overwrite current authentication? y/N"
-  read RESPONSE 
-      if [[ "$RESPONSE" == [Nn] ]]
-	then exit
-      fi
-  echo "API Key: "
-  read API_KEY  
-
-  rm -f ~/.cfze
-
-  printf "EMAIL=$EMAIL \nAPI_KEY=$API_KEY" > ~/.cfze
-  chmod 500 ~/.cfze
-
-else
-  
-  echo "API Key: "
-  read API_KEY
-
-
-  printf "EMAIL=$EMAIL \nAPI_KEY=$API_KEY" > ~/.cfze
-  chmod 500 ~/.cfze 
-fi
-}
-
-
-### All content above this line is unnecessary ###
-
-
-
 echo "Email: "
 read EMAIL
 
-
-USERNAME=$(whoami)
-
-echo "Encrypt using GPG? (y/N)"
-read RESPONSE
-
-if [[ $RESPONSE == [Nn] ]]
- then auth
-
-else 
-
 echo "Full name: "
 read REAL_NAME
+
+echo "Passphrase: "
+read PASSPHRASE
 
 cat >~/tmp.txt <<EOF
      %echo Generating a standard key
@@ -60,8 +18,9 @@ cat >~/tmp.txt <<EOF
      Name-Real: $REAL_NAME
      Name-Email: $EMAIL
      Expire-Date: 0
-     %pubring /home/$USERNAME/.cfze-gpg.pub
-     %secring /home/$USERNAME/.cfze-gpg.sec
+     Passphrase: $PASSPHRASE
+     %pubring /home/$USERNAME/.pass-gpg.pub
+     %secring /home/$USERNAME/.pass-gpg.sec
      # Do a commit here
      %commit
      %echo done
@@ -69,16 +28,19 @@ EOF
 
 gpg --batch --gen-key ~/tmp.txt
 
-rm ~/tmp.txt
+gpg --import /home/$USERNAME/.pass-gpg.pub > /dev/null 2>&1
+gpg --import /home/$USERNAME/.pass-gpg.sec > /dev/null 2>&1 
 
-gpg --import /home/$USERNAME/.cfze-gpg.pub > /dev/null 2>&1
-gpg --import /home/$USERNAME/.cfze-gpg.sec > /dev/null 2>&1 
-
-fi
 
 #GREP FOR GPG ID
-GPG_ID=$(gpg ~/.cfze-gpg.sec |  grep ssb | cut -d"/" -f2 | cut -d" " -f1)
+GPG_ID=$(gpg ~/.pass-gpg.sec |  grep ssb | cut -d"/" -f2 | cut -d" " -f1)
 
 gpg --edit-key $GPG_ID trust save
 
 pass init $GPG_ID
+
+#Clean up temp files
+
+rm /home/$USERNAME/.pass-gpg.*
+
+rm ~/tmp.txt
