e2ffa831483e6da225da31787ade697413a4189f
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-01-13 22:37:00 +0000

    Saved progress on pass-import.sh

--- pass-import.sh
+++ pass-import.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
+
 if [[ -f ~/pass_public.key && -f ~/pass_private.key ]]; then
 
 echo "Pass git repository URL: "
 read GIT_REPO
 
-gpg --import pass_public.key
+gpg --import ~/pass_public.key
 
-gpg --import pass_private.key
+gpg --import ~/pass_private.key
 
 #Get GPG key ID
 
 GPG_ID=$(gpg ~/pass_private.key |  grep ssb | cut -d"/" -f2 | cut -d" " -f1 | head -1)
 
+pass init $GPG_ID
+
+pass git init
+
+pass git remote add origin $GIT_REPO
+
 else 
 
-echo "Please name pass GPG keys as ~/pass_private.key and ~/pass_public.key"
+echo "Please name pass GPG keys as such: ~/pass_private.key and ~/pass_public.key"
 
 
 fi
